1f43336448a057c81d979e327dea17ab7052eb82
j0n4y <ian898449@gmail.com>
2026-03-10 18:11:19 +0100

    Create class Word2vec for cleaner code

--- main.py
+++ main.py
@@ -33,56 +33,59 @@ class Vocabulary:
         self.vocab_size = len(self.word2idx)
         self.corpus_size = len(self.corpus)
 
-def generate_training_pairs(vocab, window_size):
-    """Generates (target, context) pairs from corpus with the window-size specified (stores indices instead of creating one-hot vectors to optimize speed)"""
-    pairs = []
-    for i in range(len(vocab.corpus)):
-        word = vocab.corpus[i]
-        target_idx = vocab.word2idx[word]
-        start = max(0, i - window_size)
-        end = min(len(vocab.corpus), i + window_size + 1)
-        for j in range(start, end):
-            if i != j: 
-                context_word = vocab.corpus[j]
-                context_idx = vocab.word2idx[context_word]
-                pairs.append((target_idx, context_idx))       
-    return np.array(pairs)
+class Word2vec:
+    def __init__(self, vocab, window_size, embedding_dimension, init_alpha):
+        self.vocab = vocab
+        self.pairs = self._generate_training_pairs(window_size)
+        self.W1 = np.random.randn(vocab.vocab_size, embedding_dimension) * 0.01
+        self.W2 = np.random.randn(embedding_dimension, vocab.vocab_size) * 0.01
+        self.alpha = init_alpha
 
-def softmax(x):
-    e_x = np.exp(x - np.max(x)) # substracting the max to prevent overflow
-    return e_x / e_x.sum()
+    def _generate_training_pairs(self, window_size):
+        """Generates (target, context) pairs from corpus with the window-size specified (stores indices instead of creating one-hot vectors to optimize speed)"""
+        pairs = []
+        for i in range(len(self.vocab.corpus)):
+            word = self.vocab.corpus[i]
+            target_idx = self.vocab.word2idx[word]
+            start = max(0, i - window_size)
+            end = min(len(self.vocab.corpus), i + window_size + 1)
+            for j in range(start, end):
+                if i != j: 
+                    context_word = self.vocab.corpus[j]
+                    context_idx = self.vocab.word2idx[context_word]
+                    pairs.append((target_idx, context_idx))       
+        return np.array(pairs)
 
-def forward_prop(W1, W2, target_idx):
-    """Computers the forward propagation to predict context word probabilities given the index of a target word."""
-    h = W1[target_idx].reshape(-1, 1)
-    u = np.dot(W2.T, h)
-    y_pred = softmax(u)
-    return y_pred, h, u
+    def _softmax(self, x):
+        e_x = np.exp(x - np.max(x)) # substracting the max to prevent overflow
+        return e_x / e_x.sum()
 
-def back_prop(W1, W2, target_idx, context_idx, y_pred, h, u, alpha):
-    y_pred[context_idx] -= 1
-    dLdW2 = np.dot(h, y_pred.T)
-    dLdW1 = np.dot(W2, y_pred).T
-    W2 = W2 - alpha * dLdW2
-    W1[target_idx] = W1[target_idx] - alpha * dLdW1
-    return W1, W2
+    def _forward_prop(self, target_idx):
+        """Computers the forward propagation to predict context word probabilities given the index of a target word."""
+        self.h = self.W1[target_idx].reshape(-1, 1)
+        self.u = np.dot(self.W2.T, self.h)
+        self.y_pred = self._softmax(self.u)
 
-def train(epochs, W1, W2, pairs):
-    for x in range(0, epochs):
-        loss = 0
-        for target_idx, context_idx in pairs:
-            y_pred, h, u = forward_prop(W1, W2, target_idx)
-            W1, W2 = back_prop(W1, W2, target_idx, context_idx, y_pred, h, u, 0.1)
-            loss += -u[context_idx][0] + np.log(np.sum(np.exp(u)))
-        print(f"epoch {x+1}: loss = {loss}")
+    def _back_prop(self, target_idx, context_idx):
+        self.y_pred[context_idx] -= 1
+        dLdW2 = np.dot(self.h, self.y_pred.T)
+        dLdW1 = np.dot(self.W2, self.y_pred).T
+        self.W2 = self.W2 - self.alpha * dLdW2
+        self.W1[target_idx] = self.W1[target_idx] - self.alpha * dLdW1
+
+    def train(self, epochs):
+        for x in range(0, epochs):
+            loss = 0
+            for target_idx, context_idx in self.pairs:
+                self._forward_prop(target_idx)
+                self._back_prop(target_idx, context_idx)
+                loss += -self.u[context_idx][0] + np.log(np.sum(np.exp(self.u)))
+            print(f"epoch {x+1}: loss = {loss}")
 
 vocab = Vocabulary(sentences)
-pairs = generate_training_pairs(vocab, 1)
+word2vec = Word2vec(vocab, 3, 5, 0.1)
+word2vec.train(1000)
 
-# change 2 for the embedding dimension
-W1 = np.random.randn(vocab.vocab_size, 2) * 0.01
-W2 = np.random.randn(2, vocab.vocab_size) * 0.01
 
-train(100000, W1, W2, pairs)
 
 
